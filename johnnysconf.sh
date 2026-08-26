cdddc88af78daed3da765eaedb281c54ce4345ba
JohnDoemnika <corppbuzsa@proton.me>
2026-08-26 13:41:22 +0000

    Update johnnysconf.sh

--- johnnysconf.sh
+++ johnnysconf.sh
@@ -1,6 +1,8 @@
 read -p "Do you want to launch the config installer [y/n] : " start
 if [ "$start" = "y" ]; then
 mkdir ~/.config/alacritty 
+sudo pacman -S ttf-profont-nerd
+fc-cache -fv
 cp /home/$USER/alacritty-config-john/alacritty.toml ~/.config/alacritty/
 else
 exit
