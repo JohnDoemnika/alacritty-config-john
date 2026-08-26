4e2f3c5c9270b53ab207a6b7e5617079c2a45e35
JohnDoemnika <corppbuzsa@proton.me>
2026-08-26 13:28:20 +0000

    Update johnnysconf.sh

--- johnnysconf.sh
+++ johnnysconf.sh
@@ -1,7 +1,6 @@
 read -p "Do you want to launch the config installer [y/n] : " start
 if [ "$start" = "y" ]; then
 mkdir ~/.config/alacritty 
-git clone https://github.com/JohnDoemnika/alacritty-config-john.git
 cp /home/$USER/alacritty-config-john/alacritty.toml ~/.config/alacritty/
 else
 exit
